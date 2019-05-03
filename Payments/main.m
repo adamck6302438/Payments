289e587e314556e2c259b0bed24b5cd28ac03b01
Ray Kang <adamck6302438@gmail.com>
2019-05-03 11:36:39 -0400

    Setup Output and Input in main.m

--- Payments/main.m
+++ Payments/main.m
@@ -10,8 +10,14 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int money = arc4random_uniform(900)+100;
+        NSLog(@"Thank you for shopping at Acme.com Your total today is $xxx Please select your payment method: 1: Paypal, 2: Stripe, 3: Amazon");
+        char inputChar[255];
+        fgets(inputChar, 255, stdin);
+        NSString* inputString = [[NSString alloc] initWithUTF8String:inputChar];
+        inputString = [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        int operation = [inputString integerValue];
+        NSLog(@"%d", operation);
     }
     return 0;
 }
