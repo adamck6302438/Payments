6f8bd301792dffb67d35922a13083a7dbb0e0720
Ray Kang <adamck6302438@gmail.com>
2019-05-03 11:25:10 -0400

    Initial Commit

---
+++ Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by Frances ZiyiFan on 5/3/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
